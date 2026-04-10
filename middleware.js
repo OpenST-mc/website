52f4385013eb60afdd4c3a375d29e5e69876125d
Linvin <>
2026-04-10 18:08:47 +0200

    logo修复+sub_id动态meta

--- middleware.js
+++ middleware.js
@@ -1,14 +1,14 @@
-import { next } from '@vercel/edge';
-
 export default function middleware(req) {
-    const url = new URL(req.nextUrl);
+    const url = new URL(req.url);
     const ua = req.headers.get('user-agent') || '';
     const subId = url.search.replace('?', '');
     const isBot = /googlebot|bingbot|discordbot|twitterbot|facebookexternalhit|baiduspider/i.test(ua);
     if (isBot && subId.startsWith('sub-')) {
-        url.pathname = '/api/DymaticMeta';
-        return Response.rewrite(url);
+        const rewriteUrl = new URL('/api/DymaticMeta', req.url);
+        rewriteUrl.search = url.search;
+        return Response.rewrite(rewriteUrl);
     }
-
-    return next();
 }
+export const config = {
+    matcher: '/archive',
+};
